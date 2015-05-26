13a8391b08910c2db86d31755c388f462b88ec89
Ian Hutchinson <ihutchinson@agalmicventures.com>
2015-05-26 09:45:59 -0500

    Refactored FIX view script to support stdin as well

--- view_fix.py
+++ view_fix.py
@@ -5,26 +5,31 @@ import sys
 
 import fixie
 
+def printMessage(n, message):
+	if message == '':
+		return
+
+	print('%6d: %s%s' % (n, message[:100].replace(fixie.SEPARATOR, '|'), '...' if len(message) > 100 else ''))
+
+	#TODO: error handling
+	parsedMessage = fixie.parseMessage(message)
+	for k in sorted(parsedMessage.keys()):
+		name = fixie.TAG_ID_TO_NAME.get(k, '')
+		values = ', '.join(parsedMessage[k])
+		print('\t%20s [%4d] = %s' % (name, k, values))
+
+	print()
+
+def printFile(file):
+	for n, message in enumerate(file):
+		printMessage(n, message)
+
 def main():
 	if len(sys.argv) <= 1:
-		print('Ussage: view_fix.py <FILENAME>')
-		return 1
-
-	with io.open(sys.argv[1]) as fixFile:
-		for n, message in enumerate(fixFile):
-			if message == '':
-				continue
-
-			print('%6d: %s%s' % (n, message[:100], '...' if len(message) > 100 else ''))
-
-			#TODO: error handling
-			parsedMessage = fixie.parseMessage(message)
-			for k in sorted(parsedMessage.keys()):
-				name = fixie.TAG_ID_TO_NAME.get(k, '')
-				values = ', '.join(parsedMessage[k])
-				print('\t%20s [%4d] = %s' % (name, k, values))
-
-			print()
+		printFile(sys.stdin)
+	else:
+		with io.open(sys.argv[1]) as fixFile:
+			printFile(fixFile)
 
 	return 0
 
